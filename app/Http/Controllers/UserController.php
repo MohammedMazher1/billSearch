3dbf5e8d3774294a80074ecddb934adf8febf286
MohammedMazher1 <muhmmed711300411@gmail.com>
2024-02-22 12:14:50 +0300

    sync the tow project

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -14,7 +14,6 @@ class UserController extends Controller
     public function index()
     {
         $users = User::all();
-        
         return view("users.index", compact("users"));
     }
 
--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -30,7 +30,7 @@
                         </div>
                         <div>
                             <form action="{{ Route('users.destroy', $user->id) }}" method="POST">
-                                <i class="fa-solid fa-trash deleteIcon"></i>
+                                <i class="fa-solid fa-trash"></i>
                             </form>
                         </div>
                     </td>
